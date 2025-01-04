9f98a14af47c1679cda2f17c7452e888a42a5c89
Zulqarnain <zulqarnainchohan250@gmail.com>
2025-01-04 02:18:30 -0800

    r

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      {/* <h1>added one icon path</h1> */}
-      <h1>added two icon path</h1>
+      <h1>removed</h1>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -12,41 +12,41 @@ const manifestForPlugin = {
     description: "An app that can show the weather forecast for your city.",
     icons: [
       
-      // {
-      //   src: "./icon-192x192.png",
-      //   sizes: "192x192",
-      //   type: "image/png"
-      // },
+      {
+        src: "./icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
       {
         src: "./icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose:'favicon'
       },
-      // {
-      //   src: '/apple-touch-icon.png',
-      //   sizes:'180x180',
-      //   type:'image/png',
-      //   purpose:'apple touch icon',
-      // },
-      // {
-      //   src: "./icon-144x144.png",
-      //   sizes: "144x144",
-      //   type: "image/png",
-      //   purpose: "any"
-      // },
-      // {
-      //   src: "./icon-256x256.png",
-      //   sizes: "256x256",
-      //   type: "image/png",
-      //   purpose: "icon"
-      // },
-      // {
-      //   src: "./icon-384x384.png",
-      //   sizes: "384x384",
-      //   type: "image/png",
-      //   purpose: "any maskable"
-      // }
+      {
+        src: '/apple-touch-icon.png',
+        sizes:'180x180',
+        type:'image/png',
+        purpose:'apple touch icon',
+      },
+      {
+        src: "./icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "./icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "icon"
+      },
+      {
+        src: "./icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "any maskable"
+      }
     ],
     theme_color: "#181818",
     background_color: "#e8eac2",
