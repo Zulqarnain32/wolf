0df48b1978a2f6f2c05d6607ca1711699ce859e0
Zulqarnain <zulqarnainchohan250@gmail.com>
2025-01-07 09:42:31 -0800

    added dhome det ail

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+    <>
+      <div className="team-reports">
+        <div className="goals-count mt">
+          <h2>Matched  Played</h2>
+          <h2>1</h2>
+        </div>
+        <div className="goals-count mt">
+          <h2>Wins </h2>
+          <h2>0</h2>
+        </div>
+        <div className="goals-count mt">
+          <h2>Lost</h2>
+          <h2>1</h2>
+        </div>
+        <div className="goals-count mt">
+          <h2>draw</h2>
+          <h2>0</h2>
+        </div>
+        <div className="goals-count mt">
+          <h2>Goal Scored</h2>
+          <h2>2</h2>
+        </div>
+        <div className="goals-count mt">
+          <h2>Goal Conceded</h2>
+          <h2>4</h2>
+        </div>
+        <div className="goals-count mt">
+          <h2>Goal Difference</h2>
+          <h2>-1</h2>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
